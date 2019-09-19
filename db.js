724371b22ffdb1e61961b2f94584edbc94cb6b4f
a <a@as-iMac.local>
2019-09-19 06:41:23 +0300

    created database

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const { UUID, UUIDV4, BOOLEN, STRING, DECIMAL } = Sequelize;
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme-product-offerings_db');
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_product_offerings_db');
 
 const uuidDefinition = {
     type: UUID,
@@ -33,10 +33,42 @@ const Offering = conn.define('offering', {
 })
 
 Product.belongsTo(Company);
-Company.haMany(Product)
+Company.hasMany(Product)
 
-Offering.belongTo(Product);
+Offering.belongsTo(Product);
 Product.hasMany(Offering);
 
+Offering.belongsTo(Company);
+Company.hasMany(Offering);
 
-const mapAndCreate 
+
+const mapAndCreate = (items, model) => Promise.all(items.map(item => model.create(item)));
+
+const syncAndSeed = async () => {
+    await conn.sync({ force: true });
+
+    const companies = [
+        { name: 'Acme US' },
+        { name: 'Acme GLOBAL' },
+        { name: 'Acme TRI-STATE' },
+    ]
+    const [US, GLOBAL, TRISTATE] = await mapAndCreate(companies, Company);
+   
+    const products = [
+        { name: 'bar', suggestedPrice: 6,  companyId: US.id },
+        { name: 'bazz', suggestedPrice: 6, companyId: US.id},
+        { name: 'foo', suggestedPrice: 6, companyId: GLOBAL.id},
+        { name: 'quq', suggestedPrice: 6, companyId: TRISTATE.id}
+    ]
+    const [bar, bazz, foo, quq] = await mapAndCreate(products, Product);
+   
+    const offerings = [
+        { companyId: US.id, productId: bar.id },
+        { companyId: US.id, productId: bazz.id },
+        { companyId: GLOBAL.id, productId: foo.id },
+        { companyId: TRISTATE.id, productId: quq.id }
+    ]
+    const [offering1, offering2, offering3, offering4] = await mapAndCreate(offerings, Offering);
+}
+
+syncAndSeed();
